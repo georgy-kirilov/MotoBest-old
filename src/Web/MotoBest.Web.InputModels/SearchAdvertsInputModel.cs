4863b14d315c3f41fbd8cd01f7e406bea771e51a
georgy-kirilov <georgynk17@gmail.com>
2021-08-28 12:21:57 +0300

    Change Home/Search view design

--- src/Web/MotoBest.Web.InputModels/SearchAdvertsInputModel.cs
+++ src/Web/MotoBest.Web.InputModels/SearchAdvertsInputModel.cs
@@ -1,27 +1,51 @@
-﻿namespace MotoBest.Web.InputModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotoBest.Web.InputModels
 {
     public class SearchAdvertsInputModel
     {
+        [Display(Name = "Марка")]
         public int? BrandId { get; set; }
 
+        [Display(Name = "Модел")]
         public int? ModelId { get; set; }
 
+        [Display(Name = "Двигател")]
         public int? EngineId { get; set; }
 
+        [Display(Name = "Скоростна кутия")]
         public int? TransmissionId { get; set; }
 
+        [Display(Name = "Тип")]
         public int? BodyStyleId { get; set; }
 
+        [Display(Name = "Състояние")]
         public int? ConditionId { get; set; }
 
+        [Display(Name = "Евро стандарт")]
         public int? EuroStandardId { get; set; }
 
+        [Display(Name = "Цвят")]
         public int? ColorId { get; set; }
 
+        [Display(Name = "Регион")]
         public int? RegionId { get; set; }
 
+        [Display(Name = "Населено място")]
         public int? TownId { get; set; }
 
         public int Page { get; set; }
+
+        public decimal? MinPrice { get; set; }
+
+        public decimal? MaxPrice { get; set; }
+
+        public int? MinHorsePowers { get; set; }
+
+        public int? MaxHorsePowers { get; set; }
+
+        public long? MinKilometrage { get; set; }
+
+        public long? MaxKilometrage { get; set; }
     }
 }
